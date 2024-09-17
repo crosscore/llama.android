c3ec64e38375d4c7af7fb63a452defb8f443c2c4
yuu <crosscore.g@gmail.com>
2024-09-18 06:48:25 +0900

    fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 // llama.android/build.gradle.kts
-llama.android/build.gradle.kts
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
